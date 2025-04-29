d4ce586e5a376d6a6de96465f059e59221657059
BaZa06 <bazeledon@uamv.edu.ni>
2025-04-29 08:26:24 -0600

    first commit

---
+++ for2.py
@@ -0,0 +1,17 @@
+#Leer un número ingresado por el usuario
+#Mostrar la lera a por cada múmero del 1 al número
+#Ingresado por el usuario ejemplo número 3
+#a
+#aa
+#aaa
+#i es igual a la iteratura del operador
+
+def mostrarletra(numero):
+    for i in range (numero+1):
+        print(f"a"*i)
+        
+def main():
+    num = int(input("Ingrese un número: "))
+    mostrarletra(num)
+
+main()        
